8b32f31e580e2864e9d0e10a04026e96761e0ad1
Dinesh Ravi <dineshr93@gmail.com>
2023-12-09 10:39:34 +0100

    Update .ort.sh

--- .ort.sh
+++ .ort.sh
@@ -1,32 +1,22 @@
 SECONDS=0
+reportname=bill-of-materials
 if [[ $1 == "report" ]]; then
-    if [ -d *"NOTICE_"* ]; then
+    if [ -d $reportname ]; then
         echo "=============== Stage 0: Clean previous report folder ==============="
-        rm -rf *"NOTICE_"*
+        rm -rf $reportname
     fi
-    if [ -d *"bom.spdx"* ]; then
-        echo "=============== Stage 0: Clean previous report folder ==============="
-        rm -rf *"bom.spdx"*
-    fi
-    ort report --ort-file=.ort/evaluation-result.yml --output-dir=. --report-formats="PlainTextTemplate,SpdxDocument" -O "PlainTextTemplate=template.id=NOTICE_SUMMARY"
+    ort report --ort-file=.ort/evaluation-result.yml --output-dir=$reportname --report-formats="PlainTextTemplate,SpdxDocument" -O "PlainTextTemplate=template.id=NOTICE_SUMMARY"
     #"CtrlXAutomation,CycloneDx,DocBookTemplate,EvaluatedModel,FossId,FossIdSnippet,GitLabLicenseModel,HtmlTemplate,ManPageTemplate,Opossum,PdfTemplate,PlainTextTemplate,SpdxDocument,StaticHtml,TrustSource,WebApp"
 else
-    if [ -d "report" ]; then
+    if [ -d $reportname ]; then
         echo "=============== Stage 0: Clean previous report folder ==============="
-        rm -rf report
+        rm -rf $reportname
     fi
     if [ -d ".ort" ]; then
         echo "=============== Stage 0: Clean previous .ort folder ==============="
         rm -rf .ort
     fi
-    if [ -d *"NOTICE_"* ]; then
-        echo "=============== Stage 0: Clean previous report folder ==============="
-        rm -rf *"NOTICE_"*
-    fi
-    if [ -d *"bom.spdx"* ]; then
-        echo "=============== Stage 0: Clean previous report folder ==============="
-        rm -rf *"bom.spdx"*
-    fi
+
     echo "=============== Stage 1: ORT Analyze ==============="
     ort analyze --input-dir=. --output-dir=.ort
     duration=$SECONDS
@@ -47,5 +37,13 @@ else
     ort report --ort-file=.ort/evaluation-result.yml --output-dir=.ort --report-formats="WebApp"
     duration=$SECONDS
     echo "until ort report $(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
-    echo "Rectify the error present in .ort/webapp.html and run '.ort.sh report' to get final report"
+
+    echo "==========================================================================================================="
+    echo "| Rectify the error present in .ort/scan-report-web-app.html and run '.ort.sh report' to get final report |"
+    echo "==========================================================================================================="
+
+    if [ -f ".ort/scan-report-web-app.html" ]; then
+        echo "=============== Opening .ort/scan-report-web-app.html ==============="
+        open .ort/scan-report-web-app.html
+    fi
 fi
